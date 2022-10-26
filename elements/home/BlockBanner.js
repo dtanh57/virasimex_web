8bfd378ba7dcc8cf3d6eda58eca84482e8dfee37
dtanh57 <dtanh57@gmail.com>
2022-10-27 01:13:45 +0700

    1

--- elements/home/BlockBanner.js
+++ elements/home/BlockBanner.js
@@ -1,48 +1,83 @@
-import React from 'react';
-import AppAnimationWhenVisible from '../../src/AppAnimationWhenVisible';
-import {Button, Grid, Typography} from '@mui/material';
-import ForwardIcon from '@mui/icons-material/Forward';
+import React from "react";
+import AppAnimationWhenVisible from "../../src/AppAnimationWhenVisible";
+import { Button, Grid, Typography } from "@mui/material";
+import ForwardIcon from "@mui/icons-material/Forward";
 
-export default function BlockBanner({data}) {
+export default function BlockBanner({ data }) {
   return (
     <>
       {(data || []).map((item, index) => {
         return (
-          <Grid key={`${index}`} container sx={{p: 10, backgroundColor: item.backgroundColor}}>
+          <Grid
+            key={`${index}`}
+            container
+            sx={{ p: 10, backgroundColor: item.backgroundColor }}
+          >
             <Grid
               item
               lg={6}
               xs={12}
-              alignSelf='center'
-              textAlign='center'
-              sx={{display: {md: item.imgLeft ? 'block' : 'none', xs: 'block'}, mb: {xs: 2}}}
+              alignSelf="center"
+              textAlign="center"
+              sx={{
+                display: { md: item.imgLeft ? "block" : "none", xs: "block" },
+                mb: { xs: 2 },
+              }}
             >
               <AppAnimationWhenVisible>
-                <img alt={item.title} src={item.img} style={{maxWidth: '100%', height: 'auto'}} layout='fill' objectFit='contain' />
+                <img
+                  alt={item.title}
+                  src={item.img}
+                  style={{ maxWidth: "100%", height: "auto" }}
+                  layout="fill"
+                />
               </AppAnimationWhenVisible>
             </Grid>
-            <Grid item lg={6} xs={12} alignSelf='center'>
+            <Grid item lg={6} xs={12} alignSelf="center">
               <AppAnimationWhenVisible>
-                <Typography variant='h1' fontWeight='bold' sx={{mb: 2, color: item.textColor}}>
+                <Typography
+                  variant="h1"
+                  fontWeight="bold"
+                  sx={{ mb: 2, color: item.textColor }}
+                >
                   {item.title}
                 </Typography>
               </AppAnimationWhenVisible>
               <AppAnimationWhenVisible>
-                <Typography variant='h5' sx={{color: item.textColor, mb: 2}}>
+                <Typography variant="h5" sx={{ color: item.textColor, mb: 2 }}>
                   {item.content}
                 </Typography>
               </AppAnimationWhenVisible>
               {Boolean(item.btnTitle) && (
                 <AppAnimationWhenVisible>
-                  <Button variant='contained' sx={{color: item.textColor}} onClick={item.btnOnClick} endIcon={<ForwardIcon />}>
+                  <Button
+                    variant="contained"
+                    sx={{ color: item.textColor }}
+                    onClick={item.btnOnClick}
+                    endIcon={<ForwardIcon />}
+                  >
                     {item.btnTitle}
                   </Button>
                 </AppAnimationWhenVisible>
               )}
             </Grid>
-            <Grid item lg={6} xs={12} alignSelf='center' textAlign='center' sx={{display: {md: !item.imgLeft ? 'block' : 'none', xs: 'none'}}}>
+            <Grid
+              item
+              lg={6}
+              xs={12}
+              alignSelf="center"
+              textAlign="center"
+              sx={{
+                display: { md: !item.imgLeft ? "block" : "none", xs: "none" },
+              }}
+            >
               <AppAnimationWhenVisible>
-                <img alt={item.title} src={item.img} style={{maxWidth: '100%', height: 'auto'}} layout='fill' objectFit='contain' />
+                <img
+                  alt={item.title}
+                  src={item.img}
+                  style={{ maxWidth: "100%", height: "auto" }}
+                  layout="fill"
+                />
               </AppAnimationWhenVisible>
             </Grid>
           </Grid>
